61d0edb5836abe4567b6e996a5f21f3075bd8607
phu <21020658@vnu.edu.vn>
2022-03-19 15:46:45 +0700

    create a 2nd test file

---
+++ diy/diy/CommonFunc.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef COMMON_FUNCTION_H_
+#define COMMON_FUNCTION_H_
+
+#include <Windows.h>
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+
+#endif // !COMMON_FUNCTION_H
---
+++ diy/diy/Source.cpp
@@ -0,0 +1,6 @@
+#include <SDL.h>
+#include <iostream>
+#include <SDL_image.h>
+int main(int argc, char* argv[]) {
+
+}
